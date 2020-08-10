938452591aab76235015313ea017e84de9fa046d
mhooda2412 <mhooda2412@gmail.com>
2020-08-10 19:48:53 +0530

    Payment Entity created

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/PaymentEntity.java
@@ -0,0 +1,61 @@
+package com.upgrad.FoodOrderingApp.service.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+/*
+ * This Entity class represents the "payment" table in DB
+ * */
+@Entity
+@Table(name = "payment")
+
+
+/*
+ * NamedQuery to retrieve payment object from DB
+ * */
+
+@NamedQueries({
+        @NamedQuery(name = "getAllPaymentMethods", query = "SELECT p FROM PaymentEntity p")
+})
+
+public class PaymentEntity implements Serializable {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "uuid")
+    @Size(max = 200)
+    @NotNull
+    private String uuid;
+
+    @Column(name = "payment_name")
+    @Size(max = 255)
+    private String paymentName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getPaymentName() {
+        return paymentName;
+    }
+
+    public void setPaymentName(String paymentName) {
+        this.paymentName = paymentName;
+    }
+}
