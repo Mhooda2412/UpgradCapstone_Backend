53276f41afcb52f1692c908ad26e0cbb077512a3
Divya <divydaisy@gmail.com>
2020-08-12 17:25:06 +0530

    Customer and Address controller address

---
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/AddressController.java
@@ -0,0 +1,162 @@
+package com.upgrad.FoodOrderingApp.api.controller;
+
+import com.upgrad.FoodOrderingApp.api.common.Utility;
+import com.upgrad.FoodOrderingApp.api.model.*;
+import com.upgrad.FoodOrderingApp.service.businness.AddressService;
+import com.upgrad.FoodOrderingApp.service.businness.CustomerService;
+import com.upgrad.FoodOrderingApp.service.entity.AddressEntity;
+import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
+import com.upgrad.FoodOrderingApp.service.entity.StateEntity;
+import com.upgrad.FoodOrderingApp.service.exception.AddressNotFoundException;
+import com.upgrad.FoodOrderingApp.service.exception.AuthorizationFailedException;
+import com.upgrad.FoodOrderingApp.service.exception.SaveAddressException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@CrossOrigin(allowedHeaders = "*", origins = "*", exposedHeaders = ("access-token"))
+@RestController
+@RequestMapping("/")
+public class AddressController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private AddressService addressService;
+
+    /*
+    * This endpoint is used to save address in the FoodOrderingAppBackend.
+    * input - authorization field containing Bearer + access token generated from user sign-in
+    * input - SaveAddressRequest contain all user details like flat_building_name,
+    * locality, city, pincode, state_uuid
+    * Note: take the state_uuid from GET /state and pick the corresponding UUID and pass like
+    * "state_uuid": "5485eb18-a23b-11e8-9077-720006ceb890" --> for karnataka
+    * output - Success - SaveAddressResponse containing created address detail with its uuid
+    *           Failure - Failure Code  with message.
+    */
+    @RequestMapping(
+            method = RequestMethod.POST,
+            path = "/address",
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SaveAddressResponse> saveAddress(
+            @RequestHeader("authorization") final String authorization,
+            @RequestBody(required = false) final SaveAddressRequest saveAddressRequest)
+            throws AuthorizationFailedException, SaveAddressException, AddressNotFoundException {
+
+        CustomerEntity customerEntity = customerService.getCustomer(Utility.getTokenFromAuthorizationField(authorization));
+
+        StateEntity stateEntity = addressService.getStateByUUID(saveAddressRequest.getStateUuid());
+
+        AddressEntity addressEntity = new AddressEntity();
+
+        //create a new random unique uuid and set it to new Address Entity
+        addressEntity.setUuid(UUID.randomUUID().toString());
+        addressEntity.setFlatBuilNo(saveAddressRequest.getFlatBuildingName());
+        addressEntity.setLocality(saveAddressRequest.getLocality());
+        addressEntity.setCity(saveAddressRequest.getCity());
+        addressEntity.setPincode(saveAddressRequest.getPincode());
+        addressEntity.setState(stateEntity);
+
+        //Call AddressService to create a new AddressEntity
+        AddressEntity createdAddressEntity = addressService.saveAddress(addressEntity, customerEntity);
+
+        SaveAddressResponse saveAddressResponse = new SaveAddressResponse().id(createdAddressEntity.getUuid()).status("ADDRESS SUCCESSFULLY REGISTERED");
+
+        return new ResponseEntity<SaveAddressResponse>(saveAddressResponse, HttpStatus.CREATED);
+    }
+    
+    /*
+     * This endpoint is used to get all address of a customer who is signed in for the FoodOrderingAppBackend.
+     * input - authorization field containing Bearer + access token generated from user sign-in
+     * output - Success - AddressListResponse - containing list of address for single customer
+     *           Failure - Failure Code  with message.
+     */
+    @RequestMapping(
+            method = RequestMethod.GET,
+            path = "/address/customer",
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<AddressListResponse> getAddressList(
+            @RequestHeader("authorization") final String authorization)
+            throws AuthorizationFailedException {
+
+        CustomerEntity customerEntity = customerService.getCustomer(Utility.getTokenFromAuthorizationField(authorization));
+
+        List<AddressEntity> listAddressEntity = addressService.getAllAddress(customerEntity);
+
+        List<AddressList> listAddressList = null;
+        if (listAddressEntity.size() != 0) {
+            listAddressList = new ArrayList<AddressList>();
+            for (AddressEntity addressEntity : listAddressEntity) {
+                AddressListState addressListState = new AddressListState()
+                        .id(UUID.fromString(addressEntity.getState().getUuid()))
+                        .stateName(addressEntity.getState().getStateName());
+                listAddressList.add(new AddressList()
+                        .id(UUID.fromString(addressEntity.getUuid()))
+                        .flatBuildingName(addressEntity.getFlatBuilNo())
+                        .locality(addressEntity.getLocality())
+                        .city(addressEntity.getCity())
+                        .pincode(addressEntity.getPincode())
+                        .state(addressListState));
+            }
+        }
+
+        AddressListResponse addressListResponse = new AddressListResponse().addresses(listAddressList);
+
+        return new ResponseEntity<AddressListResponse>(addressListResponse, HttpStatus.OK);
+    }
+
+    /*
+     * This endpoint is used to delete the address of a customer who is signed in for the FoodOrderingAppBackend.
+     * input - authorization field containing Bearer + access token generated from user sign-in and address id in the body block
+     * output - Success - id and the message
+     *           Failure - Failure Code  with message.
+     */
+    @DeleteMapping("/address/{address_id}")
+    public ResponseEntity<DeleteAddressResponse> deleteAddress(
+            @RequestHeader("authorization") final String authorization,
+            @PathVariable("address_id") final String addressId)
+            throws AuthorizationFailedException, AddressNotFoundException {
+
+        CustomerEntity customerEntity = customerService.getCustomer(Utility.getTokenFromAuthorizationField(authorization));
+
+        //Call AddressService to search AddressEntity
+        AddressEntity addressEntity = addressService.getAddressByUUID(addressId, customerEntity);
+
+        AddressEntity deletedAddressEntity = addressService.deleteAddress(addressEntity);
+
+        DeleteAddressResponse deleteAddressResponse = new DeleteAddressResponse().id(UUID.fromString(deletedAddressEntity.getUuid())).status("ADDRESS DELETED SUCCESSFULLY");
+
+        return new ResponseEntity<>(deleteAddressResponse, HttpStatus.OK);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            path = "/states",
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<StatesListResponse> getStatesList() {
+
+        List<StateEntity> listStateEntity = addressService.getAllStates();
+
+        List<StatesList> listStatesList = null;
+
+        if (listStateEntity.size() != 0) {
+            listStatesList = new ArrayList<StatesList>();
+            for (StateEntity stateEntity : listStateEntity) {
+                listStatesList.add(new StatesList().id(UUID.fromString(stateEntity.getUuid())).stateName(stateEntity.getStateName()));
+            }
+        }
+
+        StatesListResponse statesListResponse = new StatesListResponse().states(listStatesList);
+        return new ResponseEntity<StatesListResponse>(statesListResponse, HttpStatus.OK);
+    }
+}
+
+
---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/AddressService.java
@@ -0,0 +1,94 @@
+package com.upgrad.FoodOrderingApp.service.businness;
+
+import com.upgrad.FoodOrderingApp.service.dao.AddressDao;
+import com.upgrad.FoodOrderingApp.service.dao.StateDao;
+import com.upgrad.FoodOrderingApp.service.entity.AddressEntity;
+import com.upgrad.FoodOrderingApp.service.entity.CustomerAddressEntity;
+import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
+import com.upgrad.FoodOrderingApp.service.entity.StateEntity;
+import com.upgrad.FoodOrderingApp.service.exception.AddressNotFoundException;
+import com.upgrad.FoodOrderingApp.service.exception.AuthorizationFailedException;
+import com.upgrad.FoodOrderingApp.service.exception.SaveAddressException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AddressService {
+
+    @Autowired
+    private StateDao stateDao;
+
+    @Autowired
+    private AddressDao addressDao;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public AddressEntity saveAddress(AddressEntity addressEntity, CustomerEntity customerEntity) throws SaveAddressException {
+
+        if (addressEntity.getPincode() == null || !(addressEntity.getPincode().matches("^\\d{6,6}$"))) {
+            throw new SaveAddressException("SAR-002", "Invalid pincode.");
+        }
+
+        if (addressEntity.getFlatBuilNo() == null ||
+                addressEntity.getFlatBuilNo().isEmpty() ||
+                addressEntity.getLocality() == null ||
+                addressEntity.getLocality().isEmpty() ||
+                addressEntity.getCity() == null ||
+                addressEntity.getCity().isEmpty()) {
+            throw new SaveAddressException("SAR-001", "No field can be empty.");
+        }
+
+        CustomerAddressEntity customerAddressEntity = new CustomerAddressEntity();
+        customerAddressEntity.setAddress(addressEntity);
+        customerAddressEntity.setCustomer(customerEntity);
+
+        try {
+            return addressDao.saveAddress(customerAddressEntity);
+        } catch (Exception e) {
+            throw new SaveAddressException("ADR-000", "Unknown database error while saving Address");
+        }
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public AddressEntity deleteAddress(AddressEntity addressEntity) {
+        return addressDao.deleteAddress(addressEntity);
+    }
+
+    public AddressEntity getAddressByUUID(String uuid, CustomerEntity customerEntity) throws AuthorizationFailedException, AddressNotFoundException {
+        CustomerAddressEntity customerAddressEntity = addressDao.getAddressByUUID(uuid, customerEntity.getUuid());
+        if (customerAddressEntity == null) {
+            throw new AddressNotFoundException("ANF-003", "No address by this id");
+        } else if (customerAddressEntity.getCustomer() != customerEntity) {
+            throw new AuthorizationFailedException("ATHR-004", "You are not authorized to view/update/delete any one else's address");
+        } else {
+            return customerAddressEntity.getAddress();
+        }
+    }
+
+    public List<AddressEntity> getAllAddress(CustomerEntity customerEntity) {
+
+        List<CustomerAddressEntity> listCustomerAddressEntity = addressDao.getAllCustomerAddress(customerEntity);
+        List<AddressEntity> listAddressEntity = new ArrayList<>();
+        for (CustomerAddressEntity ca : listCustomerAddressEntity) {
+            listAddressEntity.add(ca.getAddress());
+        }
+
+        return listAddressEntity;
+    }
+
+    public List<StateEntity> getAllStates() {
+        return stateDao.getAllState();
+    }
+
+    public StateEntity getStateByUUID(String uuid) throws AddressNotFoundException {
+        try {
+            return stateDao.getStateByUuid(uuid);
+        } catch (Exception e) {
+            throw new AddressNotFoundException("ANF-002", "No state by this id.");
+        }
+    }
+}
